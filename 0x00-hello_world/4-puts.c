2a813ba267d9027d32143f0aacfa2f07c7898d51
Danigy <dnlmdwrk@gmail.com>
2022-12-08 05:46:20 -0800

    Hello, puts :tada: :white_check_mark:

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -12,11 +12,8 @@ int main(void)
 
 {    
     char displayed[52];
-        
     strcpy(displayed, "\"Programming is like building a multilingual puzzle");
- 
     puts(displayed);
-    
     return (0);
-    
+
 }
