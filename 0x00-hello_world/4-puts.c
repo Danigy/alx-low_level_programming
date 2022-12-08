51238d982843ce107db0dd6f5f117c4c448bb66e
Danigy <dnlmdwrk@gmail.com>
2022-12-08 06:14:00 -0800

    Hello, puts :tada: :white_check_mark:

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
 *main - Main function of my project
 *
 *
 *Return: return 0 everytime
 */
-
 int main(void)
 {    
   puts("\"Programming is like building a multilingual puzzle");
