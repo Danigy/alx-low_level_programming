0b795737ff4dede39c2fcf0ce81fd0c87567a9cb
Danigy <dnlmdwrk@gmail.com>
2022-12-09 04:17:57 -0800

    The last digit :tada: :white_check_mark:

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  *main - Prints the last digit of random number
  *
  *Return: Return 0
 */
-
 int main(void)
 {
   int n;
   int last;
-  
   srand(time(0));
   n = rand() - RAND_MAX / 2;
   last = n % 10;
