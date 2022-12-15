1fa41df769812b3c488c5ef4a751cb02989ac38a
Danigy <dnlmdwrk@gmail.com>
2022-12-15 00:04:21 -0800

    Question #3 :tada: :white_check_mark:

---
+++ 0x03-debugging/2-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**                                                                                                
+ * main - debugging example                                                                        
+ * Return: 0                                                                                       
+*/
+int main(void)
+{
+int i;
+int j;
+for (i = 0; i < 10; i++)
+{
+j = 0;
+while (j < 10)
+{
+printf("%d", j);
+}
+printf("\n");
+}
+return (0);
+}
