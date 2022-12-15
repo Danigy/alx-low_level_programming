d75c5da38a3a69c215b921f7965fa225c41779ec
Daniel Gessese <dnlmdwrk@gmail.com>
2022-12-15 18:56:09 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,83 +1,42 @@
 #include <stdio.h>
-
 /**
-
  * main - check the code for Holberton School students.
-
  *
-
  * Return: void.
-
  */
 
-
-
 int main(void)
-  
 {
-  
 
-  
-  int i = 1;
-  
+	int i = 1;
 
-  
-  while (i <= 100)
-    
-    {
-      
-      if (i % 3 == 0 && i % 5 == 0)
-	
+	while (i <= 100)
 	{
-	  
-	  printf("FizzBuzz");
-	  
-	}
-      
-      else if (i % 3 == 0)
-	
-	{
-	  
-	  printf("Fizz");
-	  
-	}
-      
-      else if (i % 5 == 0)
-	
-	{
-	  
-	  printf("Buzz");
-	  
-	}
-      
-      else
-	
-	{
-	  
-	  printf("%i", i);
-	  
-	}
-      
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%i", i);
+		}
 
-      
-      if (i != 100)
-	
-	{
-	  
-	  putchar(' ');
-	  
+		if (i != 100)
+		{
+			putchar(' ');
+		}
+
+		i++;
 	}
-      
+	putchar('\n');
+	return (0);
 
-      
-      i++;
-      
-    }
-  
-  putchar('\n');
-  
-  return (0);
-  
-
-  
 }
