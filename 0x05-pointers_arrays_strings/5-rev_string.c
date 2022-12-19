3f8441c654bdc4fb18faa704442cf94dc77c0dd6
Daniel Gessese <dnlmdwrk@gmail.com>
2022-12-19 19:50:09 +0300

    5. A good engineer thinks in reverse and asks himself about the stylistic consequences... 🎉 ✅

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,66 +1,33 @@
 #include "main.h"
 
-
-
 /**
-
- * rev_string - Reverses a string
-
- * @s: The string to be modified
-
- *
-
- * Return: void
-
- */
-
+  * rev_string - Reverses a string
+  * @s: The string to be modified
+  *
+  * Return: void
+  */
 void rev_string(char *s)
-  
 {
-  
-  int i, c, k;
-  
-  char *a, aux;
-  
+	int i, c, k;
+	char *a, aux;
 
-  
-  a = s;
-  
+	a = s;
 
-  
-  while (s[c] != '\0')
-    
-    {
-      
-      c++;
-      
-    }
-  
+	while (s[c] != '\0')
+	{
+		c++;
+	}
 
-  
-  for (k = 1; k < c; k++)
-    
-    {
-      
-      a++;
-      
-    }
-  
+	for (k = 1; k < c; k++)
+	{
+		a++;
+	}
 
-  
-  for (i = 0; i < (c / 2); i++)
-    
-    {
-      
-      aux = s[i];
-      
-      s[i] = *a;
-      
-      *a = aux;
-      
-      a--;
-      
-    }
-  
+	for (i = 0; i < (c / 2); i++)
+	{
+		aux = s[i];
+		s[i] = *a;
+		*a = aux;
+		a--;
+	}
 }
-
