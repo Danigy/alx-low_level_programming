03531d4c9a6d0f640f94c729d71f195d376314d3
Daniel Gessese <dnlmdwrk@gmail.com>
2022-12-19 19:37:31 +0300

    3. I do not fear computers. I fear the lack of them 🎉 ✅

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,33 +1,17 @@
 #include "main.h"
 
-
-
 /**
-
- * _puts - Prints a string
-
- * @str: The string to print
-
- *
-
- * Return: void
-
- */
-
+  * _puts - Prints a string
+  * @str: The string to print
+  *
+  * Return: void
+  */
 void _puts(char *str)
-  
 {
-  
-  for (; *str != '\0'; str++)
-    
-    {
-      
-      _putchar(*str);
-      
-    }
-  
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
 
-  
-  _putchar('\n');
-  
+	_putchar('\n');
 }
