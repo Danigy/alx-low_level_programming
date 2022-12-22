5fe4988b0c666e5823956772c69f39921d9d034b
Daniel Gessese <dnlmdwrk@gmail.com>
2022-12-22 23:25:25 +0300

    9. Print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer.
- * @n: input integer.
- * Return: no return.
+ * print_number - Prints an integer
+ * @n: input integer
+ * Return: no return
  */
 void print_number(int n)
 {
