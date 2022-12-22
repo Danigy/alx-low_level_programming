ff039b427c5dd736a69642fa91616ef7449fe5b0
Danigy <dnlmdwrk@gmail.com>
2022-12-22 05:08:28 -0800

    6. Expect the best. Prepare for the worst. Capitalize on what comes :tada: :white_check_mark:

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,33 @@
 #include "main.h"
 
 /**
- *cap_string - function that capitalize first character of a word
- *@str: string to capitalize
- *Return:returns the capitalized string
+ * cap_string - function that capitalize first character of a word
+ * @str: string to capitalize
+ * Return: returns the capitalized string
 */
 
 char *cap_string(char *str)
 {
-	int index = 0;
+int index = 0;
+while (str[++index])
+{
+	while (!(str[index] >= 'a' && str[index] <= 'z'))
+		index++;
 
-	while (str[++index])
-	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-		    str[index - 1] == '\t' ||
-		    str[index - 1] == '\n' ||
-		    str[index - 1] == ',' ||
-		    str[index - 1] == ';' ||
-		    str[index - 1] == '.' ||
-		    str[index - 1] == '!' ||
-		    str[index - 1] == '?' ||
-		    str[index - 1] == '"' ||
-		    str[index - 1] == '(' ||
-		    str[index - 1] == ')' ||
-		    str[index - 1] == '{' ||
-		    str[index - 1] == '}')
-			str[index] -= 32;
-	}
-	return (str);
+	if (str[index - 1] == ' ' ||
+	    str[index - 1] == '\t' ||
+	    str[index - 1] == '\n' ||
+	    str[index - 1] == ',' ||
+	    str[index - 1] == ';' ||
+	    str[index - 1] == '.' ||
+	    str[index - 1] == '!' ||
+	    str[index - 1] == '?' ||
+	    str[index - 1] == '"' ||
+	    str[index - 1] == '(' ||
+	    str[index - 1] == ')' ||
+	    str[index - 1] == '{' ||
+	    str[index - 1] == '}')
+      str[index] -= 32;
+}
+return (str);
 }
