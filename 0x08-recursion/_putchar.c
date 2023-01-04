63461b2b37076cb2f87cfdf917ab4b2b8f37b067
Danigy <dnlmdwrk@gmail.com>
2023-01-04 12:50:26 -0800

    0. She locked away a secret, deep inside herself, something she once knew to be true...

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Writes the character c to standardout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and set to handle errno properely.
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Daniel
+ * Description: Header file containing prototypes for all functions used
+ *   in the previous task 00x07.C-Even more pointers, arrays and strings.
+ */
+
+#include <stddef.h>
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif
