29f98fce1d3d438671a565f1cb034735954bb1ef
Danigy <dnlmdwrk@gmail.com>
2023-01-06 14:21:29 -0800

    0x0A. C - argc, argv mandatory tasks

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+ /**
+  * main - A program that prints its name, followed by a new line.
+  * If you rename the progam, it'll print the new name, without
+  * having to compile it again.
+  * @argc: arguments count
+  * @argv: arguments vector
+  * Return: Always 0 on Success
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
