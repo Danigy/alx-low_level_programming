174eccc5b34389bf6230a9f9a2617e9121ff447f
Daniel Gessese <dnlmdwrk@gmail.com>
2023-01-10 10:56:44 +0300

    Update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,8 +6,9 @@ char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
-int _putchar(char c);
 char *argstostr(int ac, char **av);
+char **strtow(char *str);
+int _putchar(char c);
 int len(char *str);
 int num_words(char *str);
 
