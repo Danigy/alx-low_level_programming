9d8847a603ea177147b6310808013ef061e9a48f
Danigy <dnlmdwrk@gmail.com>
2023-02-02 03:47:38 -0800

    0. 0

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+int _putchar(char c);
+int get_endianness(void);
+void print_binary(unsigned long int n);
+unsigned int binary_to_uint(const char *b);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+#endif
