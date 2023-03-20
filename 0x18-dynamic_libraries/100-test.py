e9f071f41b276385090c614967f70b5cf9ff96f1
Danigy <dnlmdwrk@gmail.com>
2023-03-20 15:03:35 -0700

    2. Let's call C functions from Python

--- 0x18-dynamic_libraries/100-test.py
+++ 0x18-dynamic_libraries/100-test.py
@@ -1,9 +1,16 @@
+# 100-tests.py
+
 import random
 import ctypes
 
+# Load the shared object library
 cops = ctypes.CDLL('./100-operations.so')
+
+# Generate two random integers
 a = random.randint(-111, 111)
 b = random.randint(-111, 111)
+
+# Call the C functions and print the results
 print("{} + {} = {}".format(a, b, cops.add(a, b)))
 print("{} - {} = {}".format(a, b, cops.sub(a, b)))
 print("{} x {} = {}".format(a, b, cops.mul(a, b)))
